97624eca0f05dca2896b9ffe8e6fd0be5b1dc197
aaron <20100312@mail.wit.ie>
2023-05-02 17:45:20 +0100

    Implement CRUD functionality in Account screen

--- app/src/main/java/ie/wit/skytrace/ui/account/AccountFragment.kt
+++ app/src/main/java/ie/wit/skytrace/ui/account/AccountFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import ie.wit.skytrace.MainActivity
 import ie.wit.skytrace.R
 
@@ -22,6 +25,30 @@ class AccountFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
     private lateinit var storage: FirebaseStorage
+    private lateinit var getContent: ActivityResultLauncher<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let { uri ->
+                val currentUser = auth.currentUser
+                if (currentUser != null) {
+                    val profilePictureRef = storage.reference.child("profile_pictures/${currentUser.uid}.jpg")
+                    val uploadTask = profilePictureRef.putFile(uri)
+                    uploadTask.addOnSuccessListener { taskSnapshot: UploadTask.TaskSnapshot ->
+                        Toast.makeText(requireContext(), "Profile picture uploaded successfully.", Toast.LENGTH_SHORT).show()
+                        updateProfilePictureUI(profilePictureRef)
+                    }
+                        .addOnFailureListener { exception ->
+                            Toast.makeText(requireContext(), "Error uploading profile picture: ${exception.message}", Toast.LENGTH_SHORT).show()
+                        }
+                } else {
+                    Toast.makeText(requireContext(), "User not logged in.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,11 +65,14 @@ class AccountFragment : Fragment() {
 
         accountViewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
 
-        val displayNameTextView: TextView = view.findViewById(R.id.display_name_value)
+        val displayNameEditText: TextInputEditText = view.findViewById(R.id.display_name_value)
         val emailTextView: TextView = view.findViewById(R.id.email_value)
-        val phoneTextView: TextView = view.findViewById(R.id.phone_value)
-        val addressTextView: TextView = view.findViewById(R.id.address_value)
+        val phoneEditText: TextInputEditText = view.findViewById(R.id.phone_value)
+        val addressEditText: TextInputEditText = view.findViewById(R.id.address_value)
         val profileImageView: ImageView = view.findViewById(R.id.profile_picture)
+        val editProfilePictureButton: ImageButton = view.findViewById(R.id.edit_profile_picture_button)
+        val updateButton: Button = view.findViewById(R.id.edit_account_button)
+        val logoutButton: Button = view.findViewById(R.id.logout_button)
 
         val currentUser = auth.currentUser
         if (currentUser != null) {
@@ -50,10 +80,10 @@ class AccountFragment : Fragment() {
                 .get()
                 .addOnSuccessListener { document ->
                     if (document != null) {
-                        displayNameTextView.text = document.getString("displayName") ?: "Name not available"
+                        displayNameEditText.setText(document.getString("displayName") ?: "Name not available")
                         emailTextView.text = document.getString("email") ?: "Email not available"
-                        phoneTextView.text = document.getString("phone") ?: "Phone not available"
-                        addressTextView.text = document.getString("address") ?: "Address not available"
+                        phoneEditText.setText(document.getString("phone") ?: "Phone not available")
+                        addressEditText.setText(document.getString("address") ?: "Address not available")
 
                         val profilePictureRef = storage.reference.child("profile_pictures/${currentUser.uid}.jpg")
                         Glide.with(this)
@@ -62,30 +92,73 @@ class AccountFragment : Fragment() {
                             .error(R.drawable.default_profile_picture)
                             .into(profileImageView)
                     } else {
-                        displayNameTextView.text = "Name not available"
+                        displayNameEditText.setText("Name not available")
                         emailTextView.text = "Email not available"
-                        phoneTextView.text = "Phone not available"
-                        addressTextView.text = "Address not available"
+                        phoneEditText.setText("Phone not available")
+                        addressEditText.setText("Address not available")
                     }
                 }
                 .addOnFailureListener { exception ->
-                    displayNameTextView.text = "Name not available"
+                    displayNameEditText.setText("Name not available")
                     emailTextView.text = "Email not available"
-                    phoneTextView.text = "Phone not available"
-                    addressTextView.text = "Address not available"
+                    phoneEditText.setText("Phone not available")
+                    addressEditText.setText("Address not available")
                 }
         } else {
-            displayNameTextView.text = "Name not available"
+            displayNameEditText.setText("Name not available")
             emailTextView.text = "Email not available"
-            phoneTextView.text = "Phone not available"
-            addressTextView.text = "Address not available"
+            phoneEditText.setText("Phone not available")
+            addressEditText.setText("Address not available")
         }
 
-        val logoutButton: Button = view.findViewById(R.id.logout_button)
+        updateButton.setOnClickListener {
+            updateUserInformation(displayNameEditText, phoneEditText, addressEditText)
+        }
+
         logoutButton.setOnClickListener {
             auth.signOut()
             navigateToAuthScreen()
         }
+
+        editProfilePictureButton.setOnClickListener {
+            getContent.launch("image/*")
+        }
+    }
+
+    private fun updateUserInformation(displayNameEditText: TextInputEditText, phoneEditText: TextInputEditText, addressEditText: TextInputEditText) {
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val displayName = displayNameEditText.text.toString()
+            val phone = phoneEditText.text.toString()
+            val address = addressEditText.text.toString()
+
+            val userUpdates = hashMapOf(
+                "displayName" to displayName,
+                "phone" to phone,
+                "address" to address
+            )
+
+            firestore.collection("users").document(currentUser.uid)
+                .update(userUpdates as Map<String, Any>)
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), "User information updated successfully.", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(requireContext(), "Error updating user information: ${exception.message}", Toast.LENGTH_SHORT).show()
+                }
+        } else {
+            Toast.makeText(requireContext(), "User not logged in.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateProfilePictureUI(profilePictureRef: StorageReference) {
+        val profileImageView: ImageView = requireView().findViewById(R.id.profile_picture)
+
+        Glide.with(this)
+            .load(profilePictureRef)
+            .placeholder(R.drawable.default_profile_picture)
+            .error(R.drawable.default_profile_picture)
+            .into(profileImageView)
     }
 
     private fun navigateToAuthScreen() {
